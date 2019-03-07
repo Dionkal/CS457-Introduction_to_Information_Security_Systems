7a9404901819df8179acca6ce9b93eb53c599300
Dennis Kalochristianakis <dionyshs97@hotmail.com>
2019-03-07 10:39:59 +0200

    Implemented AES ECB with multiple blocks

--- SymmetricKeyExchange/crypto_demo.c
+++ SymmetricKeyExchange/crypto_demo.c
@@ -11,26 +11,46 @@
 int main(int argc, char **argv)
 {
 	int cipher_text_len, decrypted_text_len;
-
+	unsigned int numberOfBlocks;
 	unsigned char *key = (unsigned char *)"3DFFD7544A955E0580D2A67C7DC6E550";
-	unsigned char plaintext[BUFLEN];
-	unsigned char ciphertext[BUFLEN];
-	unsigned char decryptedtext[BUFLEN];
+	unsigned char plaintext[BUFLEN] = {0};
+	unsigned char ciphertext[BUFLEN] = {0};
+	unsigned char decryptedtext[BUFLEN] = {0};
 
-	strncpy((char *)&plaintext, (char *)((unsigned char *)"This is a large string that needs more blocks"), BUFLEN);
+	/* Variable initialization */
+	cipher_text_len = decrypted_text_len = numberOfBlocks = 0;
+	strncpy((char *)&plaintext, (char *)(unsigned char *)"This text is huge and needs more than 1 block", BUFLEN);
 
+	/* Determine the number of blocks required for the whole plaintext */
+	numberOfBlocks = (strlen((char *)&plaintext) < (AES_BS - 1)) ? 1 : (strlen((char *)&plaintext)) / (AES_BS - 1);
+
+	printf("Plaintext lenght: %d / Block size: %d = Number of blocks: %d\n", (int)strlen((char *)&plaintext), AES_BS - 1, numberOfBlocks);
 	/*----------------------------------------------Encrypt----------------------------------------------*/
-	cipher_text_len = aes_encrypt(plaintext, strlen((const char *)&plaintext), key, NULL, ciphertext, AES_128_ECB);
+	int plaintext_block_offset = 0;
+	for (size_t i = 0; i < numberOfBlocks; i++)
+	{
 
-	print_hex(ciphertext, cipher_text_len);
-	printf("Plaintext size: %d \t Ciphertext size: %d\n", (int)strlen((const char *)&plaintext), (int)strlen((const char *)&ciphertext));
-	printf("Ciphertext len: %d\n", cipher_text_len);
+		cipher_text_len = aes_encrypt(plaintext + plaintext_block_offset, plaintext_block_offset + (AES_BS - 1), key, NULL, ciphertext + plaintext_block_offset + (1 * i), AES_128_ECB);
+		printf("Block#%d (block offset: %d)\n:", (int)i + 1, plaintext_block_offset);
+		print_hex(ciphertext, cipher_text_len);
+		printf("Plaintext size: %d \n",
+			   plaintext_block_offset + (AES_BS - 1));
+		printf("Ciphertext len: %d\n", cipher_text_len);
+		plaintext_block_offset += AES_BS - 1;
+	}
 
 	/*----------------------------------------------Decrypt----------------------------------------------*/
-	decrypted_text_len = aes_decrypt(ciphertext, cipher_text_len, key, NULL, decryptedtext, AES_128_ECB);
+	int ciphertext_block_offset = 0;
+	for (size_t i = 0; i < numberOfBlocks; i++)
+	{
+		decrypted_text_len = aes_decrypt(ciphertext + ciphertext_block_offset, ciphertext_block_offset + AES_BS, key, NULL, decryptedtext + ciphertext_block_offset - (1 * i), AES_128_ECB);
+		ciphertext_block_offset += AES_BS;
+	}
+
 	decryptedtext[decrypted_text_len] = '\0';
-	printf("Decrypted text (%d):\n", decrypted_text_len);
+	printf("Decrypted text :\n");
 	printf("%s\n", decryptedtext);
+	printf("Decrypted text lenght: %d\n", decrypted_text_len);
 
 	return 0;
 }
