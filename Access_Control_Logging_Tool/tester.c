80ba816ec5ba0121313909fe109e339eebad0446
Dennis Kalochristianakis <dionyshs97@hotmail.com>
2019-04-07 20:41:46 +0300

    Implemented simple test program

--- Access_Control_Logging_Tool/tester.c
+++ Access_Control_Logging_Tool/tester.c
@@ -3,26 +3,41 @@
 
 int main()
 {
-	FILE *fd = fopen("test.dat", "a");
 
-	if (fd == NULL)
-	{
-		perror("Error in fopen");
-	}
+	/* create files */
+	FILE *fd1 = fopen("test1.dat", "w");
+	FILE *fd2 = fopen("test2.dat", "a");
+	FILE *fd3 = fopen("test3.dat", "a");
+	FILE *fd4 = fopen("test4.dat", "a");
+	FILE *fd5 = fopen("test5.dat", "a");
 
 	char *str = "test\n";
-	int i = fwrite(str, sizeof(char), strlen(str), fd);
-	if (i != strlen(str))
-	{
-		perror("Error in fwrite");
-	}
+	fwrite(str, sizeof(char), strlen(str), fd1);
+	fwrite(str, sizeof(char), strlen(str), fd2);
+	fwrite(str, sizeof(char), strlen(str), fd3);
+	fwrite(str, sizeof(char), strlen(str), fd4);
+	fwrite(str, sizeof(char), strlen(str), fd5);
 
-	char *str2 = "This is a second write before fclose\n";
-	i = fwrite(str2, sizeof(char), strlen(str2), fd);
-	if (i != strlen(str2))
-	{
-		perror("Error in fwrite");
-	}
-	fclose(fd);
+	char *str2 = "second test\n";
+	fwrite(str2, sizeof(char), strlen(str2), fd1);
+	fwrite(str2, sizeof(char), strlen(str2), fd2);
+	fwrite(str2, sizeof(char), strlen(str2), fd3);
+	fwrite(str2, sizeof(char), strlen(str2), fd4);
+	fwrite(str2, sizeof(char), strlen(str2), fd5);
+
+	/* close files */
+	fclose(fd1);
+	fclose(fd2);
+	fclose(fd3);
+	fclose(fd4);
+	fclose(fd5);
+
+	/* Uncomment to delete them */
+	/* remove(fd1);
+	remove(fd2);
+	remove(fd3);
+	remove(fd4);
+	remove(fd5);
+*/
 	return 0;
 }
